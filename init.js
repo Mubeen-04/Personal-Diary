71e154e7d7685720d97749de7fffc4b65485d315
Mubeen Shaikh <samubeen01@gmail.com>
2025-03-19 22:04:52 +0530

    first commit

---
+++ init.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Chat = require("./models/chat");
+
+main()
+    .then(() => {
+        console.log("Connection Successful");
+    })
+    .catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect("mongodb://localhost:27017/whatsapp");
+}
+
+const allChats = [
+    { from: "alice", to: "bob", msg: "Hey Bob, how's it going?"},
+    { from: "bob", to: "alice", msg: "Hi Alice, I'm doing great. How about you?"},
+    { from: "alice", to: "bob", msg: "All good here, just catching up on some work."},
+    { from: "john", to: "doe", msg: "Doe, did you check the report I sent?"},
+    { timestamps: { createdAt: 'created_at', updatedAt: false } } // Disable `updatedAt`
+];
+
+console.log(allChats.length); // Should be 4
+
+
+
+Chat.insertMany(allChats);
